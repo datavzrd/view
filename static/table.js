8f697174437e42d3043e8caa925472de721e86ef
fxwiegand <fxwiegand@wgdnet.de>
2022-12-15 11:18:54 +0100

    Add detail formatting

--- static/table.js
+++ static/table.js
@@ -23,7 +23,16 @@ $(document).ready(function() {
     $('#table').bootstrapTable({
         columns: columns,
         detailView: config.detail_mode,
+        detailFormatter: "detailFormatter"
     })
+
+    // Create new hidden html div to append to view and attach config to it as data attribute
+    let config_div = document.createElement("div");
+    config_div.id = "config";
+    config_div.style.display = "none";
+    config_div.setAttribute("data-config", JSON.stringify(config));
+    document.body.appendChild(config_div);
+
 
     $('#table').bootstrapTable('append', config.data);
 
@@ -60,4 +69,60 @@ function render(additional_headers, displayed_columns, table_rows, columns, conf
             shortenColumn(additional_headers.length, displayed_columns, o.title, o.ellipsis, config.detail_mode, config.header_label_length);
         }
     }
+}
+
+function detailFormatter(index, row) {
+    // Load config object from hidden div
+    let config = JSON.parse(document.getElementById("config").getAttribute("data-config"));
+
+    let cp = [];
+    let ticks = config.tick_titles;
+    let bars = config.bar_titles;
+    let displayed_columns = config.displayed_columns;
+    let hidden_columns = config.hidden_columns;
+    var html = []
+    $.each(row, function (key, value) {
+        if (!hidden_columns.includes(key) && !displayed_columns.includes(key) && key !== "linkouts" && key !== "share") {
+            if (cp.includes(key) || ticks.includes(key) || bars.includes(key)) {
+                if (cp.includes(key)) {
+                    id = `detail-plot-${index}-cp-${cp.indexOf(key)}`;
+                } else if (bars.includes(key)) {
+                    id = `detail-plot-${index}-bars-${bars.indexOf(key)}`;
+                } else {
+                    id = `detail-plot-${index}-ticks-${ticks.indexOf(key)}`;
+                }
+                var card = `<div class="card">
+                   <div class="card-header">
+                     ${key}
+                   </div>
+                   <div class="card-body">
+                     <div id="${id}"></div>
+                   </div>
+                 </div>`;
+                html.push(card);
+            } else if (config.heatmap_titles.includes(key)) {
+                id = `heatmap-${index}-${config.heatmap_titles.indexOf(key)}`;
+                var card = `<div class="card">
+                  <div class="card-header">
+                    ${key}
+                  </div>
+                  <div id="${id}" class="card-body">
+                    ${value}
+                  </div>
+                </div>`;
+                html.push(card);
+            } else {
+                var card = `<div class="card">
+                   <div class="card-header">
+                     ${key}
+                   </div>
+                   <div class="card-body">
+                    ${value}
+                   </div>
+                 </div>`;
+                html.push(card);
+            }
+        }
+    })
+    return `<div class="d-flex flex-wrap">${html.join('')}</div>`
 }
